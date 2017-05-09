52e024e3d0d2940cd84594bf1fefa517aa8cf90b
lizi1234560 <1873209744@qq.com>
2017-05-09 09:31:11 +0800

    blog-lizi

---
+++ blog/views.py
@@ -0,0 +1,95 @@
+import markdown
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
+from comments.forms import CommentForm
+from .models import Post, Category
+
+"""
+使用下方的模板引擎方式。
+def index(request):
+    return HttpResponse("欢迎访问我的博客首页！")
+"""
+
+"""
+使用下方真正的首页视图函数
+def index(request):
+    return render(request, 'blog/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页'
+    })
+"""
+
+
+def index(request):
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 1)
+    page = request.GET.get('page')
+
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+"""
+增加了评论功能后需要相应地更新 detail 函数，更新后的函数见下边。
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    return render(request, 'blog/detail.html', context={'post': post})
+
+"""
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        TocExtension(slugify=slugify)
+    ])
+    post.body = md.convert(post.body)
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {'post': post,
+               'toc': md.toc,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year, created_time__month=month)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def search(request):
+    q = request.GET.get('q')
+    error_msg = ''
+
+    if not q:
+        error_msg = '请输入关键词'
+        return render(request, 'blog/index.html', {'error_msg': error_msg})
+
+    post_list = Post.objects.filter(title__icontains=q)
+    return render(request, 'blog/index.html', {'error_msg': error_msg,
+                                               'post_list': post_list})
